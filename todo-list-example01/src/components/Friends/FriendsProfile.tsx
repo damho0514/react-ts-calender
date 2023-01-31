a19dc0fc0acd9f3714fd89dd03ae9de22d65505f
damho0514 <damho0514@gmail.com>
2023-01-31 22:59:31 +0900

    feat: Add user profile updated

---
+++ todo-list-example01/src/components/Friends/FriendsProfile.tsx
@@ -0,0 +1,85 @@
+import { useRecoilValue } from 'recoil';
+import styled, { css } from 'styled-components';
+import defaultImage from '../../assets/images/profileDefaultImage.png';
+import useWeather from '../../hooks/useWeather';
+import { selectUserById } from '../../stores/friends';
+import selectedProfileState from '../../stores/selectedProfile';
+
+const FriendsProfile = () => {
+  const selectedId = useRecoilValue(selectedProfileState);
+  console.log('selectedId', selectedId);
+  const [selectedProfile] = useRecoilValue(selectUserById(selectedId));
+  console.log('selectedProfile', selectedProfile);
+  const { degree, icon } = useWeather();
+  return (
+    <Wrapper>
+      <div>
+        <ProfileImage image={selectedProfile.profileImage} />
+        <Content>
+          <div>{selectedProfile.name}</div>
+          <div>{selectedProfile.statusMessage}</div>
+        </Content>
+      </div>
+      <Weather>
+        <img src={icon} />
+        <p>{degree}</p>
+      </Weather>
+    </Wrapper>
+  );
+};
+export default FriendsProfile;
+
+const Wrapper = styled.div`
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const ProfileImage = styled.div<{ image: string }>`
+  width: 46px;
+  height: 46px;
+  border-radius: 50%;
+  ${({ image }) =>
+    image.length > 2
+      ? css`
+          background-image: url(${image});
+        `
+      : css`
+          background-image: url(${defaultImage});
+          border: 1px solid ${({ theme }) => theme.palette.mono.gray_ed};
+        `}
+  background-size: contain
+`;
+
+const Content = styled.div`
+  margin-left: 10px;
+  & > div:first-child {
+    font-size: 18px;
+    font-weight: 700;
+  }
+  & > div:last-child {
+    font-size: 13px;
+    font-weight: 500;
+    color: ${({ theme }) => theme.palette.mono.gray_99};
+    margin-top: 4px;
+  }
+`;
+
+const Weather = styled.div`
+  height: 46px;
+
+  & > img {
+    height: 46px;
+  }
+
+  & > p {
+    font-weight: 600;
+    color: ${({ theme }) => theme.palette.mono.gray_99};
+  }
+`;
